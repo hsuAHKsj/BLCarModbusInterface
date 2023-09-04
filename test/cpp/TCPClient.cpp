f0776b7dce9e42159e820cb1f7a0f08ba5dea4df
Edwin Wong <1043562599@qq.com>
2023-09-04 06:08:50 -0700

    [feat] 文件结构变更，将原cppmodbus接口放置在test文件夹下方便测试

---
+++ test/cpp/TCPClient.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+class TCPClient {
+private:
+    int client_socket;
+    sockaddr_in server_address;
+
+public:
+    TCPClient(const std::string& host, int port) {
+        // Create socket
+        client_socket = socket(AF_INET, SOCK_STREAM, 0);
+        if (client_socket == -1) {
+            std::cerr << "Failed to create socket: " << strerror(errno) << std::endl;
+            throw std::runtime_error("Socket creation failed");
+        }
+
+        memset(&server_address, 0, sizeof(server_address));
+        server_address.sin_family = AF_INET;
+        server_address.sin_port = htons(port);
+        server_address.sin_addr.s_addr = inet_addr(host.c_str());
+
+        // Connect to server
+        if (connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == -1) {
+            std::cerr << "Failed to connect: " << strerror(errno) << std::endl;
+            close(client_socket);
+            throw std::runtime_error("Connection failed");
+        }
+    }
+
+    ~TCPClient() {
+        if (client_socket != -1) {
+            close(client_socket);
+        }
+    }
+
+    void disconnect() {
+        if (client_socket != -1) {
+            close(client_socket);
+            client_socket = -1;
+        }
+    }
+
+    std::string sendAndReceive(const std::string& message) {
+        if (send(client_socket, message.c_str(), message.size(), 0) == -1) {
+            std::cerr << "Failed to send data: " << strerror(errno) << std::endl;
+            disconnect();
+            throw std::runtime_error("Failed to send data");
+        }
+
+        char buffer[1024] = {0};
+        int bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
+        if (bytes_received == -1) {
+            std::cerr << "Failed to receive data: " << strerror(errno) << std::endl;
+            disconnect();
+            throw std::runtime_error("Failed to receive data");
+        }
+
+        return std::string(buffer);
+    }
+};
+
+int main() {
+    try {
+        TCPClient client("192.168.1.62", 8888);
+
+        while (true) {
+            std::string message;
+            std::cout << "请输入要发送的数据 (输入 'exit' 退出): ";
+            std::getline(std::cin, message);
+
+            if (message == "exit") {
+                break;
+            }
+
+            std::string response = client.sendAndReceive(message);
+            std::cout << "收到服务器的回复: " << response << std::endl;
+        }
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
